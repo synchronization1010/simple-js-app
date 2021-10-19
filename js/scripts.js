d561f3e8a9a2a5983eaf6579fe6274e3ad5bcb65
synchronization1010 <85970124+synchronization1010@users.noreply.github.com>
2021-10-19 12:40:13 -0500

    For Loop w/ Height check

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 let pokemonList = [
-    {name:'Bulbasaur', height:0.7, type: ['grass','poison']},
-    {name:'Ivysaur', height:1, type: ['grass','poison']},
-    {name:'Venusaur', height:2, type: ['grass','poison']},
+    {name:'Bulbasaur', height:0.7, type: ['grass', 'poison']},
+    {name:'Ivysaur', height:1, type: ['grass', 'poison']},
+    {name:'Venusaur', height:2, type: ['grass', 'poison']},
     {name:'Charmander', height:0.6, type: ['fire']},
     {name:'Charmeleon', height:1.1, type: ['fire']},
-    {name:'Charizard', height:1.7, type: ['fire','flying']}
-    ]
+    {name:'Charizard', height:1.7, type: ['fire', 'flying']}
+    ];
+
+document.write("Here are the pokemon in your pokedex: <br>");
+
+for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height < 1) {
+        document.write("Name: " + pokemonList[i].name + " (Height: " + pokemonList[i].height + " Size: small) <br>")
+    } else if (pokemonList[i].height > 1.5) {
+        document.write("Name: " + pokemonList[i].name + " (Height: " + pokemonList[i].height + " Size: large) <br>")
+    } else {
+        document.write("Name: " + pokemonList[i].name + " (Height: " + pokemonList[i].height + " Size: average) <br>")
+    }
+}
+
