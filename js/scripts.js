32d4efa9a2b9bbf85df50bfefca9047f275a2685
synchronization1010 <85970124+synchronization1010@users.noreply.github.com>
2021-11-02 17:31:16 -0500

    Wrapped array in IIFE. Updated loop for access

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,31 @@
-let pokemonList = [
-    {name:'Bulbasaur', height:0.7, type: ['grass', 'poison']},
-    {name:'Ivysaur', height:1, type: ['grass', 'poison']},
-    {name:'Venusaur', height:2, type: ['grass', 'poison']},
-    {name:'Charmander', height:0.6, type: ['fire']},
-    {name:'Charmeleon', height:1.1, type: ['fire']},
-    {name:'Charizard', height:1.7, type: ['fire', 'flying']}
-    ];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name:'Bulbasaur', height:0.7, type: ['grass', 'poison']},
+        {name:'Ivysaur', height:1, type: ['grass', 'poison']},
+        {name:'Venusaur', height:2, type: ['grass', 'poison']},
+        {name:'Charmander', height:0.6, type: ['fire']},
+        {name:'Charmeleon', height:1.1, type: ['fire']},
+        {name:'Charizard', height:1.7, type: ['fire', 'flying']}
+        ];
+        
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+    
+    return {
+        add: add,
+        getAll: getAll()
+    };
+})()
+
 
 document.write("Here are the pokemon in your pokedex: <br>");
 
-pokemonList.forEach(function(pokemon){
+pokemonRepository.getAll.forEach(function(pokemon){
     if (pokemon.height < 1) {
         document.write("Name: " + pokemon.name + " (Height: " + pokemon.height + " Size: small) <br>")
     } else if (pokemon.height > 1.5) {
