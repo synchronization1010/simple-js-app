b88b20ac01dfb271013befd04deefbbf3d34926b
synchronization1010 <85970124+synchronization1010@users.noreply.github.com>
2021-11-10 14:53:52 -0600

    Created addListItem and showDetails Functions / Created Buttom / Styled with CSS / Log clicks to console

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,65 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-        {name:'Bulbasaur', height:0.7, type: ['grass', 'poison']},
-        {name:'Ivysaur', height:1, type: ['grass', 'poison']},
-        {name:'Venusaur', height:2, type: ['grass', 'poison']},
-        {name:'Charmander', height:0.6, type: ['fire']},
-        {name:'Charmeleon', height:1.1, type: ['fire']},
-        {name:'Charizard', height:1.7, type: ['fire', 'flying']}
-        ];
-        
-    function add(pokemon) {
-        pokemonList.push(pokemon);
+  let repository = [
+    {name:'Bulbasaur', height:0.7, type: ['grass', 'poison']},
+    {name:'Ivysaur', height:1, type: ['grass', 'poison']},
+    {name:'Venusaur', height:2, type: ['grass', 'poison']},
+    {name:'Charmander', height:0.6, type: ['fire']},
+    {name:'Charmeleon', height:1.1, type: ['fire']},
+    {name:'Charizard', height:1.7, type: ['fire', 'flying']}
+  ];
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
     }
+  }
 
-    function getAll() {
-        return pokemonList;
-    }
-    
-    return {
-        add: add,
-        getAll: getAll()
-    };
-})()
+  function getAll() {
+    return repository;
+  }
 
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener('click', function(event) {
+      showDetails(pokemon);
+    })
+  }
 
-document.write("Here are the pokemon in your pokedex: <br>");
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
 
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+  };
+})();
+
+pokemonRepository.add(
+  {name: "Pikachu", height: 0.3, types: ["electric"]}
+);
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
+
+/* --OLD FUNCTION FOR REFERENCE--
 pokemonRepository.getAll.forEach(function(pokemon){
     if (pokemon.height < 1) {
         document.write("Name: " + pokemon.name + " (Height: " + pokemon.height + " Size: small) <br>")
@@ -34,3 +69,4 @@ pokemonRepository.getAll.forEach(function(pokemon){
         document.write("Name: " + pokemon.name + " (Height: " + pokemon.height + " Size: average) <br>")
     }
 })
+*/
